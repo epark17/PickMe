ca3fc1a5ab76a8c48d042bdcf2a0908ed4a7c0a0
Esther Park <estherspark91@gmail.com>
2018-09-23 20:20:16 -0400

    fixed! but now the pictures look terrible

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,18 @@ App = {
   hasVoted: false,
 
   init: () => {
+    // Load CDs
+    $.getJSON('../candidates.json', function (data) {
+      let petsRow = $('#petsRow');
+      let petTemplate = $('#petTemplate');
+      for (i = 0; i < data.length; i++) {
+        petTemplate.find('.panel-title').text(data[i].name);
+        petTemplate.find('img').attr('src', data[i].picture);
+        petsRow.append(petTemplate.html());
+      }
+    });
+
+
     return App.initWeb3();
   },
 
